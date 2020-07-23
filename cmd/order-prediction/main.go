b5205d75d0478ff7eeddc0a41f1f8cb5f6b75b8a
Varun Gupta1 <varun.gupta1@SW-L-P00576.local>
2020-07-23 21:28:43 +0530

    Analytics function added

--- cmd/order-prediction/main.go
+++ cmd/order-prediction/main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/antchfx/jsonquery"
+	"github.com/elgs/gojq"
 	"github.com/varungupte/BootCamp_Team3/pkg/orders"
 	"os"
-	"strings"
-
+	"strconv"
 )
 
 func main() {
@@ -31,13 +30,66 @@ func main() {
 	jsonFile.Write(jsonData)
 	jsonFile.Close()
 
-	analytics(string(jsonData))
+	analyticsPopularFood(string(jsonData))
+	analyticsPopularDishCitywise(string(jsonData),"San Francisco")
 }
 
-func analytics(jsonData string){
+func analyticsPopularFood(jsonData string){
 
-	doc, _ := jsonquery.Parse(strings.NewReader(jsonData))
-	fmt.Println(doc)
-	fmt.Println("cooooool")
-	//fmt.Println(err)
+	//Using gojq library https://github.com/elgs/gojq#gojq
+	parser, _ := gojq.NewStringQuery(jsonData)
+
+
+	//Popular restaurants(based on number of orders)
+	var m = make(map[string]int)
+	for i := 0; i < 1000; i++ {
+		var f string
+		f = "["+strconv.Itoa(i)+"].Restau.Name"
+		q,_:=parser.Query(f)
+		m[q.(string)]=m[q.(string)]+1
+	}
+	// Iterating map
+	var res string
+	maxres:=-1
+	for i, p := range m {
+		if p > maxres{
+			res = i
+			maxres = p
+		}
+	}
+	fmt.Println("The most popular restaurant is:-",res)
+	fmt.Println(maxres," times, food was order from here.")
+}
+
+func analyticsPopularDishCitywise(jsonData string,cityName string){
+
+	//Using gojq library https://github.com/elgs/gojq#gojq
+	parser, _ := gojq.NewStringQuery(jsonData)
+
+
+	//Popular Dish Areawise(In a particular User City, which is the dish maximum order)
+	var m = make(map[string]int)
+	for i := 0; i < 1000; i++ {
+		var f string
+		f = "["+strconv.Itoa(i)+"].User.City"
+		q,_:=parser.Query(f)
+		if q==cityName{
+			var d string
+			d = "["+strconv.Itoa(i)+"].DishName"
+			dishName,_:=parser.Query(d)
+			m[dishName.(string)]=m[dishName.(string)]+1
+		}
+
+	}
+	// Iterating map
+	var res string
+	maxres:=-1
+	for i, p := range m {
+		if p > maxres{
+			res = i
+			maxres = p
+		}
+	}
+	fmt.Println("The most popular dish in ",cityName," is:-")
+	fmt.Println(res)
 }
